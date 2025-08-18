2b1fb536797e756ed84f629426d094b2648dee4a
Diessy <100002916+DiessyS@users.noreply.github.com>
2025-08-17 21:24:45 -0300

    refactor: InfusionCryptoType enum with full names

--- lib/enum/infusion_crypto_type.dart
+++ lib/enum/infusion_crypto_type.dart
@@ -1,13 +1,14 @@
 enum InfusionCryptoType {
-  chaCha20('ChaCha20', 0xe97bed2),
-  aesGcm128('AES-GCM-128', 0x56050b),
-  aesGcm192('AES-GCM-192', 0x8b0f6b),
-  aesGcm256('AES-GCM-256', 0xfa80b5);
+  chaCha20('ChaCha20', 'ChaCha20-Poly1305', 0x1c6b3f),
+  aesGcm128('AES 128', 'AES-128-GCM', 0x56050b),
+  aesGcm192('AES 192', 'AES-192-GCM', 0x8b0f6b),
+  aesGcm256('AES 256', 'AES-256-GCM', 0xfa80b5);
 
   final String name;
+  final String fullName;
   final int id;
 
-  const InfusionCryptoType(this.name, this.id);
+  const InfusionCryptoType(this.name, this.fullName, this.id);
 
   static InfusionCryptoType fromId(int? id) {
     return InfusionCryptoType.values.firstWhere(
