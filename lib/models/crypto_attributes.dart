2edf864719af3d13366879c0653d1423dabbbeb3
Diessy <100002916+DiessyS@users.noreply.github.com>
2026-02-11 19:36:50 -0300

    fix: iv can be embed in some cases

--- lib/models/crypto_attributes.dart
+++ lib/models/crypto_attributes.dart
@@ -2,14 +2,14 @@ import 'dart:typed_data';
 
 class CryptoAttributes {
   final Uint8List data;
-  final Uint8List iv;
   final Uint8List derivedKey;
   final Uint8List aad;
+  Uint8List? iv;
 
   CryptoAttributes({
     required this.data,
-    required this.iv,
     required this.derivedKey,
     required this.aad,
+    this.iv,
   });
 }
